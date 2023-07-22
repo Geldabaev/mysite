70b2cd2e9c3306b6d1a86f571688f8cdce7527a1
yunus <geldabaev@mail.ru>
2023-07-23 02:22:56 +0300

    html

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseNotFound, Http404
+from .models import Post
+from django.utils import timezone
 
 
 def index(request):
-    return render(request, "blog/index.html", {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_dat')
+    return render(request, 'blog/index.html', {'posts': posts})
 
 
 def categ(request, catid):
+    if request.GET:
+        print(request.GET)  # http://127.0.0.1:8000/cats/2/?name=Yunus&first=Geldabaev
+    if int(catid) == 100:  # ручная генерация 404
+        raise Http404
+    if int(catid) == 50:
+        return redirect('home')  # redirect 302 перемещена временно
+    if int(catid) == 10:
+        return redirect('home', permanent=True)  # redirect 301 перемещена на постоянно
     return HttpResponse(f"<h1>page{catid}</h1>")
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from blog.views import *
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,6 @@ urlpatterns = [
 ]
 
 handler404 = pageNotFound
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
