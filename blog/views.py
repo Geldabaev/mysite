3ff2e8445bd892c302515e1c4149d8e4e6c79dca
yunus <geldabaev@mail.ru>
2023-07-23 03:39:55 +0300

    css

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 
 def index(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_dat')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/index.html', {'posts': posts})
 
 
